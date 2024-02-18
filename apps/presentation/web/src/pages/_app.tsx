29285afda292cb6b5b53dcdb2fee4f856fc7ddb4
unkly <u7gyuuu7gi@gmail.com>
2024-02-18 20:44:49 +0900

    add:nextAuthのひな型作成

--- apps/presentation/web/src/pages/_app.tsx
+++ apps/presentation/web/src/pages/_app.tsx
@@ -1,16 +1,19 @@
+import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-const App = ({ Component, pageProps }: AppProps) => (
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
   <>
     <Head>
-      <title />
+      <title>recipeaceful</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1"
       />
     </Head>
-    <Component {...pageProps} />
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   </>
 )
 
